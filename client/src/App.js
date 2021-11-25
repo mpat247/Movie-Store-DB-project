668bfdef8d4e3c77c5749dabf6cbc10b91cf609a
mario <mario-ianniello@hotmail.com>
2021-11-24 23:52:20 -0500

    you play too much

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,33 @@ import { Button, ButtonGroup } from "@chakra-ui/react";
 import axios from 'axios';
 
 function App() {
+  let titles = [];
+  async function componentDidMount() {
+    try {
+       this.titles = await axios.get("http://localhost:3010/getTitles");
+      } catch (err){
+        console.log(err);
+      }
+  }
 
   async function createTables() {
     try {
       const response = await axios.put("http://localhost:3010/createTables",{});
       console.log(response);
+      alert(response.data);
+    } catch (err){
+      console.log(err);
+      
+    }
+  
+  }
+  
+
+  async function populateTables() {
+    try {
+      const response = await axios.put("http://localhost:3010/populateTables",{});
+      console.log(response);
+      alert(response.data);
     } catch (err){
       console.log(err);
     }
@@ -19,6 +41,7 @@ function App() {
     try {
       const response = await axios.post("http://localhost:3010/drop",{});
       console.log(response);
+      alert(response.data);
     } catch (err){
       console.log(err);
     }
@@ -28,10 +51,42 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>ONLINE MOVIE STORE</h1>
-        <Button colorScheme="teal" variant="outline" onClick={createTables} >Create Tables</Button>
-        <Button colorScheme="teal" variant="outline" onClick={dropTables} >Drop Tables</Button>
+        
+        <div className="menu">
+          <Button colorScheme="teal" variant="outline" onClick={createTables} >Create Tables</Button>
+          &nbsp;
+
+          &nbsp;
+          <Button colorScheme="teal" variant="outline" onClick={dropTables} >Drop Tables</Button>
+        </div>
+        <div className="title">
+          <h1>ψ ☠̶̧̢̻̯̞̹̠͙̜̗̞͖̤̦͍̯̫͓̣̰̯̦̳̗̹̳͍͕͔̜̖̤̱̺̥̙̤̬̟̬͚͍̬̌̂̿́͜ͅ ̶̠̈́̇̔͂̊̌͋̋́͆̈́͛̂̆̾̓̍̅̀̽͑̌̅̀͒̔͒́̒͋͛̄͗͆͑͂̕͠͝͝ŏ̷̫̫̿̍̍͜ņ̶̡̧̛̬̲͉̖͈̗̟͕̮̲͇̳͈͇̘͉̼̜̙͚̮̰͍̠̩͋͐̑̆̀͌̋̀͐̀̏͗̏͑̒̋̆̆̏̌͒̐͐̕̕͜͝͝ļ̸̢̛̻͉̝̼͙͔͎̤̞̹͆̓̋̓͒̌͑́̄̄̓͑̓́̃̈̏̉̄̈̂̒̂̆̎̋̈̓͗̎́̒̽̈́͂́͗́͘͘͘͠͝͝ȋ̸̡̢̡̡̧̨̨̨̛̛̱̯̯̦̩͍̻̻̘̺̠̬͇͚̝͉̺͈̜̹̝̦͖͕̰̙̫͇͌̒͒̓̾̑́̂̽̋͛̍̎̅̂̉͒͊̓̄̇̃͋̓̊̓̌͗̍̆͂̕̚͠͝͠͠͝n̸̡̨̛͕͈̜̮͇̳̜͓͓̞̙̻͇͙̦̯̝̬̼̬͂̍͑̉͛̅͗͒̀̉̅̊̎̄͐̃́̌̊̑̇̿̅̎̽̓͗͂͗͊̍̀͆̆̎̎̄́̊͛̃͘̕͝͝͝͝ͅe̴̡̧̫̳̖͖̖̩̺̙̯͎͈̪̼͙͎͋͑̀̑̔͆̋̉̏̿̐͊̿͆̏͐̈́̃̑̊̑͊̋̐̌̀͋̓̔̐̋̑̂͌̿͘̚͜͠͝͝͝͠ ̷̧̨̨̼̭̺̠̫͖̗̼̖͇̞̼̣̰͍̘̜̬̟̠̼͇̤̙̮̘̝͙̹̫̻͙̜̦̦̼͋̒̏̿̀̾̓̈́́̚͜͝m̸̱̖͉̠̪͖͔̼̥͕͔͉̥͔͇̮͕̠̝̤̹͈̙̦͈͉͙̾̇̊̆̓̽̅̈͋̿̇̊͝ͅͅő̴̡̨͎̞͎͚̪̣͖͍͍͇̭͇̱̲̣̪̼͙̮̤̙̺̩͆͗̂̄͒̈̍͜͜͠͠͠ͅv̶̡͉͇͇̟͚̬͔͙͒̏͐̉̓̀͊̉̾̈͆̋́̉͛͗͆͒͑͑̅͑̈̓̓̐̑̈́̽̈̄̾̈́̌͌́́̋̉̃͑̚͝͠͝ͅì̶̛̮͍̬͕̝̜̗̜̺̠̘̼̬͂̅̎̀̈́̇̏̓̔͐̑͗̄̑̂͒͂̂͜ę̶̧̧̡̨̛̛͇̪̫͎͚̮͉̰͉͖̞̟͕̙̫̫͍͖̠͙͎̯̪̹̈͂̋̀̈̒̿͑͌͗̊͋̋́̒̑́͗͋́̉̿̔͋͌̆́͌͂̿̈́̈́̋͒̐̈́̔̿̿̔͒͛͑̃̋̌ͅ ̵̧̧̧̛̳̰͉̻͈̝͚͎̰̟̫̖̣̥̋̓͐́̊͆̀̒̃̔̓̎̾̊̈́̀̆̿͊͛̊̔̍̄̈́́̌̓͘̕͘ͅs̵̛̼̱̠͚̝͙̰̓̅͋̓́̂̄̒̌̉̿̿͌̑͛̓̇̒̃̇̀͊̀̉̆̾̈̀́̕t̶̢̨̡̢̧̳͕͈͎̫̺̠̜̠̙̺͙̳͇̰͙̼̘͎̮̥̥̙̩͙̜̥͍̱͈̹͖̳̱̫̯͇́͌̌̂͆̌͂̋̈́͒͌͊̈̃̀̒̀͋̌̌͊̌͒̈́̀̔͑̉̎͂̿̈̋̈́̋͘͘͝ͅớ̷̭̲̥̪͖̙̠̮̯̀̐͐͑̂̿̂͑̋̽̓̎͛̈̒̓̈̇̈́͂́̾̋̈̐̀̿͊͘̕͘͜͜͝r̶̨̡̜̝̼̘͍̰̻͎̝̺̠̯̯͙̹͍̰̗͌̉͌͑̇͜ͅe̶̡̡̡̯̲̫̮̭̝͕͍͎͍̬̖̲̼͎̘̘͙̺͉̗͎̻͔̻̝̣̜̪̘̞̓̒͊̕͜ͅ ̸̮̻͂͊̔̇͋͑̍̄́̚͘͘͜͝⛥̸̱̙͔͖̘͖͎͂̕͜͜ͅ ψ</h1>
+        </div>
+        <div className="menu">
+          <Button colorScheme="teal" variant="outline" onClick={populateTables} >Populate Tables</Button>
+          &nbsp;
+
+          &nbsp;
+          <Button colorScheme="teal" variant="outline" onClick={dropTables} >Query Tables</Button>
+        </div>
       </header>
+      <body>
+
+      {titles.map(title=> (
+<div>
+<img src={title.art}/>
+<h5>{title.Title_Name}</h5>
+</div>
+
+      ))}
+
+
+        <h5>Wolf Of Wall Street</h5>
+        <h5>Django</h5>
+        <h5>The Purge</h5>
+        <h5>South Park</h5>
+        <h5>simpsons</h5>
+      </body>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -21,72 +21,256 @@ app.post('/drop', async (req, res) => {
 
 
     try {
-        conn = await oracledb.getConnection(config)
-    
-        await conn.execute('DROP TABLE CUSTOMER CASCADE CONSTRAINTS');
-        await conn.execute('DROP TABLE EPISODE CASCADE CONSTRAINTS');
-        await conn.execute('DROP TABLE FAMOUSNAMES CASCADE CONSTRAINTS');
-        await conn.execute('DROP TABLE FEATURES CASCADE CONSTRAINTS');
-        await conn.execute('DROP TABLE GENRE CASCADE CONSTRAINTS');
-        await conn.execute('DROP TABLE POINTOFSALE CASCADE CONSTRAINTS');
-        await conn.execute('DROP TABLE MOVIE CASCADE CONSTRAINTS');
-        await conn.execute('DROP TABLE PRICE CASCADE CONSTRAINTS');
-        await conn.execute('DROP TABLE OWN_PERIOD CASCADE CONSTRAINTS');
-        await conn.execute('DROP TABLE SEASON CASCADE CONSTRAINTS');
-        await conn.execute('DROP TABLE TITLE CASCADE CONSTRAINTS');
-        await conn.execute('DROP TABLE TITLE CASCADE CONSTRAINTS');
-        await conn.execute('DROP TABLE CONTRIBUTES CASCADE CONSTRAINTS');
-        res.send("dropped sucessfully ")
-      } catch (err) {
-        console.log('Ouch!', err)
-      } finally {
-        if (conn) { // conn assignment worked, need to close
-          await conn.close()
-          
-        }
+      conn = await oracledb.getConnection(config)
+
+      let promises = [];
+
+      promises.push(conn.execute('DROP TABLE CUSTOMER CASCADE CONSTRAINTS').catch(e => console.log(e)));
+      promises.push(conn.execute('DROP TABLE EPISODE CASCADE CONSTRAINTS').catch(e => console.log(e)));
+      promises.push(conn.execute('DROP TABLE FAMOUSNAMES CASCADE CONSTRAINTS').catch(e => console.log(e)));
+      promises.push(conn.execute('DROP TABLE FEATURES CASCADE CONSTRAINTS').catch(e => console.log(e)));
+      promises.push(conn.execute('DROP TABLE GENRE CASCADE CONSTRAINTS').catch(e => console.log(e)));
+      promises.push(conn.execute('DROP TABLE POINTOFSALE CASCADE CONSTRAINTS').catch(e => console.log(e)));
+      promises.push(conn.execute('DROP TABLE MOVIE CASCADE CONSTRAINTS').catch(e => console.log(e)));
+      promises.push(conn.execute('DROP TABLE PRICE CASCADE CONSTRAINTS').catch(e => console.log(e)));
+      promises.push(conn.execute('DROP TABLE OWN_PERIOD CASCADE CONSTRAINTS').catch(e => console.log(e)));
+      promises.push(conn.execute('DROP TABLE SEASON CASCADE CONSTRAINTS').catch(e => console.log(e)));
+      promises.push(conn.execute('DROP TABLE TVSHOW CASCADE CONSTRAINTS').catch(e => console.log(e)));
+      promises.push(conn.execute('DROP TABLE TITLE CASCADE CONSTRAINTS').catch(e => console.log(e)));
+      promises.push(conn.execute('DROP TABLE CONTRIBUTES CASCADE CONSTRAINTS').catch(e => console.log(e)));
+
+      await Promise.all(promises);
+      res.send("dropped sucessfully ")
+    } catch (err) {
+      console.log('Ouch!', err)
+    } finally {
+      if (conn) { // conn assignment worked, need to close
+        await conn.close()
+
       }
+    }
   })
 
 
 app.put('/populateTables', async (req, res) => {
-    try {
-        conn = await oracledb.getConnection(config)
+  try {
+    conn = await oracledb.getConnection(config)
     
-        const result = await conn.execute(
-            
-            )
-    
-        console.log(result.rows[0])
-        res.send("populated sucessfully ")
-      } catch (err) {
-        console.log('Ouch!', err)
-      } finally {
-        if (conn) { // conn assignment worked, need to close
-          await conn.close()
-          
-        }
-      }
-  })
+    const result = await conn.execute(
+      `INSERT INTO CUSTOMER
+      VALUES ('MARIO','ASDF','CANADA','MAR@ASD.COM','416 990 4664');
+      
+      INSERT INTO CUSTOMER
+      VALUES ('JAMES','ASDF','CANADA','J@ASD.COM','416 552 5967');
+      
+      INSERT INTO CUSTOMER
+      VALUES ('KELLY','ASDF','CANADA','K@ASD.COM','905 548 6894');
+      
+      
+      INSERT INTO TITLE 
+      VALUES ('1','The Purge',2012);
+      
+      INSERT INTO TITLE 
+      VALUES
+      ('2','south park', 2002);
+      
+      INSERT INTO TITLE 
+      VALUES
+      ('3','wolf of wall street', 2017);
+      
+      INSERT INTO TITLE 
+      VALUES
+      ('4','django',2011);
+      
+      INSERT INTO TITLE 
+      VALUES
+      ('5','simpsons',1999);
+      
+      INSERT INTO FAMOUSNAMES
+      VALUES('DICAPRIO','ACTOR');
+      
+      INSERT INTO FAMOUSNAMES
+      VALUES ('FOXX','ACTOR');
+      
+      INSERT INTO CONTRIBUTES
+      VALUES('1','3','DICAPRIO');
+      
+      INSERT INTO CONTRIBUTES
+      VALUES ('2','4','FOXX');
+      
+      INSERT INTO CONTRIBUTES
+      VALUES ('3','4','DICAPRIO');
+      
+      
+      
+      
+      
+      INSERT INTO TVSHOW
+      VALUES
+      ('2','SOUTH PARK', '2');
+      
+      INSERT INTO TVSHOW
+      VALUES
+      ('5','SIMPSONS', '4');
+      
+      
+      INSERT INTO SEASON(TITLE_ID, SEASON_ID, SEASON_NUM) 
+      VALUES ('2','1','1');
+      
+      INSERT INTO SEASON
+      VALUES ('2','2','2');
+      
+      INSERT INTO SEASON
+      VALUES ('5','3','1');
+      
+      INSERT INTO SEASON
+      VALUES ('5','4','2');
+      
+      INSERT INTO SEASON
+      VALUES ('5','5','3');
+      
+      INSERT INTO SEASON 
+      VALUES ('5','6','4');
+      
+      
+      INSERT INTO EPISODE (SEASON_ID,EPISODE_ID,EPISODE_NUM)
+      VALUES ('1','1','1');
+      
+      INSERT INTO EPISODE 
+      VALUES ('1','2','2');
+      
+      INSERT INTO EPISODE
+      VALUES ('1','3','3');
+      
+      INSERT INTO EPISODE 
+      VALUES ('2','4','1');
+      
+      INSERT INTO EPISODE
+      VALUES ('3','5','1');
+      
+      INSERT INTO EPISODE
+      VALUES ('4','6','1');
+      
+      INSERT INTO EPISODE
+      VALUES ('5','7','1');
+      
+      INSERT INTO EPISODE
+      VALUES ('6','8','1');
+      
+      INSERT INTO MOVIE
+      VALUES ('1','THE PURGE','90');
+      
+      
+      INSERT INTO MOVIE
+      VALUES ('3','wolf of wall street','160');
+      
+      INSERT INTO MOVIE
+      VALUES ('4','django','80');
+      
+      
+      
+      INSERT INTO GENRE
+      VALUES ('1','HORROR');
+      
+      INSERT INTO GENRE
+      VALUES ('2', 'ACTION');
+      
+      INSERT INTO GENRE
+      VALUES ('3','COMEDY');
+      
+      INSERT INTO GENRE
+      VALUES ('4','HALLOWEEN');
+      
+      INSERT INTO GENRE
+      VALUES ('5','CHRISTMAS');
+      
+      INSERT INTO GENRE
+      VALUES ('6','THRILLER')
+      
+      INSERT INTO GENRE 
+      VALUES ('7','DRAMA')
+      
+      
+      INSERT INTO FEATURES 
+      VALUES ('1','1');
+      
+      INSERT INTO FEATURES
+      VALUES ('2','3');
+      
+      INSERT INTO FEATURES
+      VALUES ('6','1');
+      
+      INSERT INTO FEATURES
+      VALUES ('7','3');
+      
+      INSERT INTO FEATURES
+      VALUES('3','3');
+      
+      INSERT INTO FEATURES
+      VALUES ('2','4');
+      
+      INSERT INTO FEATURES
+      VALUES ('3','5');
+      
+      INSERT INTO POINTOFSALE
+      VALUES ('1','MARIO','1','VISA',DATE'2021-07-01');
+      
+      INSERT INTO PRICE
+      VALUES ('1', '5.00');
+      
+      INSERT INTO PRICE
+      VALUES ('3', '7.00');
+      
+      INSERT INTO PRICE
+      VALUES ('4', '4.00');
+      
+      INSERT INTO OWN_PERIOD
+      VALUES(DATE'2021-07-01', StartDate+14);
+      
+
+      `
+    )
+
+    console.log(result.rows[0])
+    res.send("populated sucessfully ")
+  } catch (err) {
+    console.log('Ouch!', err)
+  } finally {
+    if (conn) { // conn assignment worked, need to close
+      await conn.close()
+
+    }
+  }
+})
 
 app.put('/createTables', async (req, res) => {
-    
+
         conn = await oracledb.getConnection(config)
-        await conn.execute(
+
+        let promises = []
+
+        promises.push(conn.execute(
           `CREATE TABLE CUSTOMER(
                     User_Name VARCHAR2(25) PRIMARY KEY,
                     User_password VARCHAR2(25),
                     Region VARCHAR2(20),
                     User_Email VARCHAR2(25),
                     User_phone_number VARCHAR2(20)
-                )`);
-        await conn.execute(
+                )`).catch(e => {
+                  console.log("customer", e);
+                }));
+
+        promises.push(conn.execute(
           `    
                 CREATE TABLE TITLE(
                     Title_id VARCHAR2(30) PRIMARY KEY,
-                    TitleName VARCHAR2(30)
+                    TitleName VARCHAR2(30),
+                    ART VARCHAR2(2048)
                 )
-                `);
-        await conn.execute(
+                `).catch(e => {
+                  console.log("title", e);
+                }));
+        promises.push(conn.execute(
           `
                 CREATE TABLE MOVIE(
                     Title_id VARCHAR2(30) REFERENCES TITLE(Title_id)
@@ -95,8 +279,10 @@ app.put('/createTables', async (req, res) => {
                     MovieRuntime VARCHAR2(8),
                     PRIMARY KEY (Title_id)
                 )
-                `);
-        await conn.execute(
+                `).catch(e => {
+                  console.log("movie", e);
+                }));
+        promises.push(conn.execute(
           `
                 CREATE TABLE TVSHOW(
                     Title_id VARCHAR2(30) REFERENCES TITLE(Title_id)
@@ -105,8 +291,10 @@ app.put('/createTables', async (req, res) => {
                     NumOfSeasons VARCHAR2(3),
                     PRIMARY KEY (Title_id)
                 )
-                `);
-        await conn.execute(
+                `).catch(e => {
+                  console.log("tvshow", e);
+                }));
+        promises.push(conn.execute(
           `
                 CREATE TABLE SEASON(
                     Title_id VARCHAR2(30) REFERENCES TVSHOW(Title_id)
@@ -114,8 +302,10 @@ app.put('/createTables', async (req, res) => {
                     Season_id VARCHAR2(30) PRIMARY KEY,
                     Season_Num VARCHAR2(30)
                 )
-                `);
-        await conn.execute(
+                `).catch(e => {
+                  console.log("season", e);
+                }));
+                promises.push(conn.execute(
           `
                 CREATE TABLE EPISODE(
                     Season_id VARCHAR2(30) REFERENCES SEASON(Season_id)
@@ -124,11 +314,13 @@ app.put('/createTables', async (req, res) => {
                     Episode_Num VARCHAR2(30),
                     Episode_Name VARCHAR2(30)
                 )
-                `);
+                `).catch(e => {
+                  console.log("episode", e);
+                }));
     
     
     
-        await conn.execute(
+                promises.push( conn.execute(
           `
                 CREATE TABLE POINTOFSALE(
                     TransactionNO NUMBER PRIMARY KEY,
@@ -136,8 +328,10 @@ app.put('/createTables', async (req, res) => {
                     Title_id VARCHAR2(30) REFERENCES TITLE(Title_id),
                     PayMethod VARCHAR2(10),
                     startdate DATE 
-                )`);
-         await conn.execute(
+                )`).catch(e => {
+                  console.log("pos", e);
+                }));
+                promises.push( conn.execute(
            `
                 CREATE TABLE PRICE(
                     Title_id VARCHAR2(30) REFERENCES TITLE(Title_id),
@@ -145,52 +339,64 @@ app.put('/createTables', async (req, res) => {
                     
                     PRIMARY KEY(Title_id)
                 )
-                `);
+                `).catch(e => {
+                  console.log("price", e);
+                }));
     
-        await conn.execute(
+                promises.push( conn.execute(
           `
                 CREATE TABLE OWN_PERIOD(
-                    Startdate DATE REFERENCES POINTOFSALE(startdate) PRIMARY KEY ,
+                    startdate DATE PRIMARY KEY ,
                     enddate DATE
                 )
-                `);
-        await conn.execute(
+                `).catch(e => {
+                  console.log("ownperiod", e);
+                }));
+                promises.push( conn.execute(
           `
                 CREATE TABLE FAMOUSNAMES(
                     FamousName VARCHAR2(30) PRIMARY KEY,
                     FamousProfession VARCHAR2(30)
                 )
-                `);
-        await conn.execute(
+                `).catch(e => {
+                  console.log("fn", e); 
+                }));
+                promises.push( conn.execute(
           `
                 CREATE TABLE CONTRIBUTES(
                     Con_ID VARCHAR(10) PRIMARY KEY,
                     Title_id VARCHAR2(30) REFERENCES TITLE(Title_id),
                     FamousName VARCHAR2(30) REFERENCES FAMOUSNAMES(FamousName)
                 )
-                `);
+                `).catch(e => {
+                  console.log("contri", e);
+                }));
     
     
     
     
-        await conn.execute(
+                promises.push( conn.execute(
           `
                 CREATE TABLE GENRE(
                     Category_id VARCHAR2(30) PRIMARY KEY,
                     CategoryName VARCHAR2(30)
                 )
-                `);
-        await conn.execute(
+                `).catch(e => {
+                  console.log("genre", e);
+                }));
+                promises.push( conn.execute(
           `
                 CREATE TABLE FEATURES(
                     Category_id VARCHAR2(30),
                     Title_id VARCHAR2(30) REFERENCES TITLE(Title_id),
                         PRIMARY KEY(Category_id, Title_id)
                 )
-                `);
+                `).catch(e => {
+                  console.log("feat", e);
+                }));
     
     
-      
+                await Promise.all(promises);
 
             
            
@@ -198,7 +404,9 @@ app.put('/createTables', async (req, res) => {
         
     
                 await conn.close()
-        res.send("created tables sucessfully")
+       
+       
+                res.send("created tables sucessfully")
        
          
           
@@ -209,27 +417,27 @@ app.put('/createTables', async (req, res) => {
 
 app.get('/getTitles', async (req, res) => {
 
-    try {
-        conn = await oracledb.getConnection(config)
-    
-        const result = await conn.execute(
-          'select * from Title',
-          []
-          
-        )
-    
-        console.log(result.rows[0])
-        res.send(result.rows)
-      } catch (err) {
-        console.log('Ouch!', err)
-      } finally {
-        if (conn) { // conn assignment worked, need to close
-          await conn.close()
-          
-        }
-      }
+  try {
+    conn = await oracledb.getConnection(config)
 
-    res.send()
+    const result = await conn.execute(
+      'select * from Title',
+      []
+
+    )
+
+    console.log(result.rows[0])
+    res.send(result.rows)
+  } catch (err) {
+    console.log('Ouch!', err)
+  } finally {
+    if (conn) { // conn assignment worked, need to close
+      await conn.close()
+
+    }
+  }
+
+  res.send()
 })
 
 app.listen(port, () => {
